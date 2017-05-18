994f9a39207e79375276d5766f5b2a3198ad460a
Jesse de Does <jesse.dedoes@inl.nl>
2017-05-18 16:13:57 +0200

    .

--- src/main/scala/Nederlab.scala
+++ src/main/scala/Nederlab.scala
@@ -11,19 +11,46 @@ object Nederlab
 {
   def concordances(cql: String) =
   {
+    val schema = null
     val client = new NederlabClient
+    
     for (h <- client.getHits(cql).iterator().asScala)
-      
-      
+      println( createRow(h,null)._1 )
   }
   
-  def createRow(kwic:Hit, meta:Map[String,String]): Row = 
+  def createSchema(fields:List[String], metaFields: List[String]):StructType = 
 		{
-				val tokenValues  = kwic.properties.asScala.toList.map(
-						s => (kwic.tokens.map((t:Token) => t.tokenProperties.asScala.filter());
-
+			val tokenFields = fields.map(fieldName => StructField(fieldName, new ArrayType(StringType,false), nullable = true))
+			val metaFieldz = metaFields.map(fieldName => StructField(fieldName, StringType, nullable = true))
+			val extraFields = List(
+								StructField("hitStart", IntegerType, nullable=false),
+								StructField("hitEnd", IntegerType, nullable=false))
+			val schema = StructType(extraFields ++ tokenFields ++ metaFieldz)
+			println("Schema:" + schema)
+			schema
+		}
+  
+  def createRow(hit:Hit, schema:StructType): (Row,StructType) = 
+		{
+        val tokenFields = hit.knownPrefixes.asScala.toList
+				val tokenValues  = 
+				  tokenFields.map( 
+						s => (
+						       hit.tokens.asScala.flatMap( 
+						           (t:Token) => t.tokenProperties.asScala.filter(_.prefix==s).map(_.value)
+						    )))
+				
+				val meta = hit.document.getMetadata.asScala		    
 				val metaKeys = (meta.keys.toList.sorted)
 				val metaValues = metaKeys.map(s => meta(s))
-				Row.fromSeq(kwic.getHitStart :: kwic.getHitEnd :: tokenValues ++ metaValues)
+				(
+				    Row.fromSeq(hit.startPosition :: hit.endPosition :: tokenValues ++ metaValues),
+				    if (schema==null) createSchema(tokenFields,metaKeys) else schema
+				)
 		} 
+  
+  def main(args:Array[String])
+  {
+    concordances(  """[t_lc="deliberatie" | tc_lc="deliberaties"]""" )
+  }
 }
