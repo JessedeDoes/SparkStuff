4a8de98035712c36d05250391ecc9e12defbc8a9
Jesse de Does <jesse.dedoes@inl.nl>
2017-05-19 11:37:45 +0200

    .

--- src/main/scala/RDF.scala
+++ src/main/scala/RDF.scala
@@ -6,15 +6,15 @@ import org.apache.jena.sparql.engine.http.QueryEngineHTTP
 class RDF
 {
   val serviceURI = "http://dbpedia.org/sparql"
- 
+  val diamantURI = "http://svprre02:8080/fuseki/tdb/sparql"
   
-  def query(queryString: String) = 
+  def query(service: String, queryString: String) = 
 	{
 		val query = QueryFactory.create(queryString) ;
 		val start = System.currentTimeMillis();
 		try  
 		{
-		  val qexec = new QueryEngineHTTP(serviceURI, query)
+		  val qexec = new QueryEngineHTTP(service, query)
 		  
 		  //val qexec = QueryExecutionFactory.create(query, dataset)
 			if (query.isSelectType)
@@ -55,5 +55,17 @@ class RDF
 object RDF
 {
     val example = "construct  {?x a \"<http://dog>\" } where {?x a ?Concept} LIMIT 100"
-    def main(args:Array[String]) = (new RDF).query(example)
+    val att = """
+         construct { ?a <word> ?wf . ?a <quote> ?q . ?a <b> ?b . ?a <e> ?e   . ?a <t> ?t } 
+where 
+{ 
+  ?f <http://www.ivdnt.org/diamant#attestation> ?a .
+  ?f <http://lemon-model.net/lemon#writtenRep> ?wf .
+  ?a a <http://www.ivdnt.org/diamant#Attestation> . 
+ ?a <http://www.ivdnt.org/diamant#beginIndex> ?b .
+?a <http://www.ivdnt.org/diamant#endIndex> ?e .
+ ?a <http://www.ivdnt.org/diamant#text> ?t .
+ ?t <http://www.ivdnt.org/diamant#quotationText> ?q
+} limit 100"""
+    def main(args:Array[String]) = { val r = (new RDF); r.query(r.diamantURI, att) }
 }
