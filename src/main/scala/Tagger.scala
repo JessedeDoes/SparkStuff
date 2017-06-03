e3032872531a8a8a5d51abdedc16381867426860
jessededoes <dedoes@xs4all.nl>
2017-06-03 21:32:27 +0200

    .

--- src/main/scala/Tagger.scala
+++ src/main/scala/Tagger.scala
@@ -10,22 +10,27 @@ object babTagger extends Tagger
    def urlFor(text:String) = s"http://inl-labs.inl.nl/succeed/text?input=${text}&format=text&tagger=bab-tagger&output=raw" 
    def taggedDocument(text:String):Elem = XML.load(urlFor(text))
    
-   def tag(text:String) =
+   def getTextButNotIn(x:Node,tagName:String):String =
+   {
+       if (x.isInstanceOf[Text])
+         x.text
+       else 
+         x.child.filter(y => y.label != tagName).map(n => getTextButNotIn(n,tagName)).mkString("")
+   }
+   
+   def tag(text:String):Map[String,Array[String]] =
    {
      val d = taggedDocument(text)
      val z = (d \\ "w").map(w => 
        List(
-           ("word", w.text), 
+           ("word", getTextButNotIn(w,"interp")), 
            ("lemma", (w \ "@lemma").text), 
            ("pos", (w \ "@type").text)).toMap)
        
        
        val properties = z.flatMap(x => x.keys).toSet.toList
-       val l = properties.map((p:String) => (p -> z.map(m => m(p)))).toMap
+       val l = properties.map( (p:String) => (p -> z.map(m => m(p)).toArray)).toMap
        l  
-       // .map((p:String) => z.map(m => m(p)))
-       // z.map(p => z.map 
-       // map(p => (0 to z.size-1).map(i => z(i)
    }
    
    def main(args:Array[String]):Unit = (tag("Ghy syt een esel die syn sinnen hevet verlooren")).foreach(println)
