810daff8bbaaed70af723ae093880554eaaf5361
jessededoes <dedoes@xs4all.nl>
2017-06-03 14:45:00 +0200

    .

--- src/main/scala/Tagger.scala
+++ src/main/scala/Tagger.scala
@@ -8,7 +8,20 @@ trait Tagger
 object babTagger extends Tagger
 {
    def urlFor(text:String) = s"http://inl-labs.inl.nl/succeed/text?input=${text}&format=text&tagger=bab-tagger&output=raw" 
-   def tag(text:String):Elem = XML.load(urlFor(text))
+   def taggedDocument(text:String):Elem = XML.load(urlFor(text))
    
-   def main(args:Array[String]):Unit = println(tag("Ghy sy een esel die syn sinnen hevet verlooren"))
+   def tag(text:String) =
+   {
+     val d = taggedDocument(text)
+     val z = (d \\ "w").map(w => 
+       List(
+           ("word", w.text), 
+           ("lemma", (w \ "@lemma").text), 
+           ("pos", (w \ "@pos").text)).toMap)
+       
+       
+       val a = z.flatMap(x => x.keys).toSet
+       // map(p => (0 to z.size-1).map(i => z(i)
+   }
+   def main(args:Array[String]):Unit = println(taggedDocument("Ghy sy een esel die syn sinnen hevet verlooren"))
 }
