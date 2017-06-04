7a29e73d51e3920309d8b0a73aee233607d87883
jessededoes <dedoes@xs4all.nl>
2017-06-04 14:08:04 +0200

    .

--- src/main/scala/Tagger.scala
+++ src/main/scala/Tagger.scala
@@ -43,7 +43,7 @@ object babTagger extends Tagger
    def main(args:Array[String]):Unit = 
     {
       val tagged = tag("""" Ghy syt een eesel, die syn sinnen hevet verloeren. Swyght!
-        INDEN beginne schiep Godt den Hemel, ende de Aerde.
+        In den beginne schiep Godt den Hemel, ende de Aerde.
  De Aerde nu was woest ende ledich, ende duysternisse was op den afgront: ende de Geest Godts sweefde op de Wateren.
  Ende Godt seyde: Daer zy Licht: ende daer wert Licht.
 Ende Godt sach het Licht, dat het goet was: ende Godt maeckte scheydinge tusschen het Licht, ende tusschen de duysternisse.  
