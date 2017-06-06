267c5e29652505c2c42a8d80cbf50d7c5d246c75
jessededoes <dedoes@xs4all.nl>
2017-06-06 12:12:50 +0200

    .

--- src/main/scala/ResolveSynonyms.scala
+++ src/main/scala/ResolveSynonyms.scala
@@ -1,3 +1,21 @@
+import word2vec.{Vectors,Util, Distance}
+
+trait VectorSpace
+{
+   lazy val vectors:Vectors = ???
+   type vector = Array[Float]
+   def similarity(v1: Array[Float], v2: Array[Float]):Double =
+      Distance.cosineSimilarity(v1,v2)
+   def embedding(word: String) = vectors.getVector(word)
+   def norm(v: vector):Double = Math.sqrt(v.map(x => x*x).sum)
+}
+
+object DbnlVectors extends VectorSpace
+{
+   lazy val vectors = Vectors.readFromFile("/home/jesse/workspace/Diamant/Vectors/dbnl.vectors.bin")
+
+
+}
 object ResolveSynonyms
 {
    val idZin = "M089253"
