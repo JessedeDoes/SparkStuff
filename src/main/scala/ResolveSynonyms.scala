00bc2399f38dde85a1a313b6c0a8dcf63c198157
jessededoes <dedoes@xs4all.nl>
2017-06-07 22:46:40 +0200

    .

--- src/main/scala/ResolveSynonyms.scala
+++ src/main/scala/ResolveSynonyms.scala
@@ -10,24 +10,24 @@ trait VectorSpace
    {
       try
       {
-         val z:T = x();
-         if (z == null) None else Some(z)
+         val z:T = x()
+         Option(z)
       } catch { case e:Exception =>  None }
    }
 
    def lift2[T,S](f: (T,T) => S):(Option[T],Option[T]) => Option[S] =
       { (a:Option[T],b:Option[T]) => (a,b) match { case (Some(x), Some(y)) => Some( f(x,y)); case _ => None } }
 
-   def preprocess(s: String) = Tokenizer.tokenize( entities.substitute(s.replaceAll("<[^<>]*>", "") )).map(t => t.token)
+   def preprocess(s: String):Array[String] = Tokenizer.tokenize( entities.substitute(s.replaceAll("<[^<>]*>", "") )).map(t => t.token)
    def embedding(word: String):Option[vector] = { attemptNotNull(() => vectors.getVector(word)) }
    def norm(v: vector):Double = Math.sqrt(v.map(x => x*x).sum)
    def normalized(v: vector): vector = v.map(f => f / norm(v).asInstanceOf[Float]) // gaat de optimizer deze herhaling weghalen ?
-   def sum(v1: vector, v2:vector):vector = (0 to v1.length -1).map(i => v1(i) + v2(i)).toArray // silly
+   def sum(v1: vector, v2:vector):vector = v1.indices.map(i => v1(i) + v2(i)).toArray // silly
    def average(l: Seq[vector]):vector = normalized(l.reduce(sum))
 
    def averageVector(l: Seq[String]):Option[vector] =
    {
-      val okeetjes =  l.map(embedding).filter(_ != None).map(_.get)
+      val okeetjes =  l.map(embedding).filter(_.isDefined).map(_.get)
       if (okeetjes.isEmpty)
          { //Console.err.println("!No words found for:" + l.toList);
              None }
@@ -53,7 +53,7 @@ trait VectorSpace
 
 object DbnlVectors extends VectorSpace
 {
-   override lazy val vectors = Vectors.readFromFile("/home/jesse/workspace/Diamant/Vectors/dbnl.vectors.bin")
+   override lazy val vectors:Vectors = Vectors.readFromFile("/home/jesse/workspace/Diamant/Vectors/dbnl.vectors.bin")
 }
 object ResolveSynonyms
 {
