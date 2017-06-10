2f2792011a1b144a7f5613fe83dd45713c132a3d
jessededoes <dedoes@xs4all.nl>
2017-06-10 10:00:09 +0200

    .

--- src/main/scala/KBKwic.scala
+++ src/main/scala/KBKwic.scala
@@ -12,31 +12,25 @@ object KBKwic
   {
     override def toString():String = (f"${left}%80s") + "\t"  + hit + "\t" + right
   }
-  
-  def concordance(query:String,document:Node):List[Kwic] = 
+
+  def concordance(query:TextQuery, text:String):List[Kwic] =
   {
-    val tokens = tokenize(document.text)
-    // println(tokens.toList)
-    val matchPositions = (0 to tokens.length-1).toList.filter(i => tokens(i).token.toLowerCase == query.toLowerCase())
-    //println(matchPositions)
-    def slice = (a:Int,b:Int) => tokens.slice(Math.max(0,a),Math.min(b,tokens.length-1)).toList.map(
-        t => t.leading + t.token + t.trailing).mkString(" ")
-    def getMatch(p:Int) = Kwic(slice(p-window,p), tokens(p).token, slice(p+1,p+window+1))
-    matchPositions.map(getMatch)
-  }
-  
-  def concordance(query:TextQuery, document:Node):List[Kwic] = 
-  {
-    val tokens = tokenize(document.text)
+    val tokens = tokenize(text)
     val terms = SRU.termsIn(query).map(_.toLowerCase)
     // println(tokens.toList)
     val matchPositions = (0 to tokens.length-1).toList.filter(i => terms.contains(tokens(i).token.toLowerCase))
     //println(matchPositions)
     def slice = (a:Int,b:Int) => tokens.slice(Math.max(0,a),Math.min(b,tokens.length-1)).toList.map(
-        t => t.leading + t.token + t.trailing).mkString(" ")
+      t => t.leading + t.token + t.trailing).mkString(" ")
     def getMatch(p:Int) = Kwic(slice(p-window,p), tokens(p).token, slice(p+1,p+window+1))
     matchPositions.map(getMatch)
   }
+
+  def concordance(query:String,document:Node):List[Kwic] = concordance(SingleTerm(query), document)
+
+  
+  def concordance(query:TextQuery, document:Node):List[Kwic] = concordance(query, document.text)
+
   
   def concordanceFile(query:String, fileName:String):List[Kwic] = concordance(query,XML.load(fileName))
   
@@ -45,8 +39,10 @@ object KBKwic
   
   def concordanceDir(query:String, dirName:String):Unit = concordancesDir(query,dirName).foreach(println)
   
-  def concordance(query:String, url:String):List[Kwic] = concordance(query,XML.load(url))
-  def concordance(query:TextQuery, url:String):List[Kwic] = concordance(query,XML.load(url))
+  def concordanceURL(query:String, url:String):List[Kwic] = concordance(query,XML.load(url))
+
+  def concordanceURL(query:TextQuery, url:String):List[Kwic] = concordance(query,XML.load(url))
+
   def kwicResults(s:String) =
     
     for ((id,metadataRecord) <- matchingDocumentIdentifiers(s))
@@ -59,7 +55,7 @@ object KBKwic
       split.par.foreach(
            x =>  
              for ((id,metadataRecord) <- x)
-             { println(KBKwic.concordance(t, id)) }
+             { println(KBKwic.concordanceURL(t, id)) }
       )
   }
   
