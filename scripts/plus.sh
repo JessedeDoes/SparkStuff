3c6b92c6215ab163b4c17ebc101f577835cabad3
jessededoes <dedoes@xs4all.nl>
2017-06-17 18:45:37 +0200

    .

---
+++ scripts/plus.sh
@@ -0,0 +1,4 @@
+NBDIR=~/Downloads/spark-notebook-0.7.0-scala-2.11.8-spark-2.1.0-hadoop-2.8.0/
+export CLASSPATH_OVERRIDES=/home/jesse/workspace/SparkStuff/target/scala-2.11/SparkStuff-assembly-1.0.jar
+cd $NBDIR
+./bin/spark-notebook
