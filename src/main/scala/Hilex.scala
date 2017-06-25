702fc19b279eaa949feb5175d30113231ad36b66
jessededoes <dedoes@xs4all.nl>
2017-06-25 23:28:26 +0200

    .

--- src/main/scala/Hilex.scala
+++ src/main/scala/Hilex.scala
@@ -134,7 +134,7 @@ object hilexQueries {
     val q =
       s"""
         select 
-                       lemma_id, analyzed_wordform_id, wordform 
+          lemma_id, analyzed_wordform_id, wordform
         from 
             ${dataSchema}.analyzed_wordforms a, 
             ${dataSchema}.wordforms w 
@@ -387,6 +387,32 @@ object hilexQueries {
   )
   val romanOrArabic = (s: Sense) => List("roman", "arabic").contains(s.sense_type) || s.parent_sense_id == null
   // as in: concat(sql"select id from USERS where id in ", values(Seq(1,2))).as[Int]
+
+  val createSimple =
+    """
+      |create temporary table part_of_group
+      |select distinct
+      |        analyzed_wordforms.analyzed_wordform_id, documents.persistent_id
+      |from
+      |        wordform_groups,
+      |        token_attestations,
+      |        analyzed_wordforms,
+      |        documents
+      |where
+      |        wordform_groups.document_id = token_attestations.document_id and
+      |        wordform_groups.onset = token_attestations.start_pos and
+      |        analyzed_wordforms.analyzed_wordform_id = token_attestations.analyzed_wordform_id and
+      |        documents.document_id=token_attestations.document_id;
+      |alter table part_of_group add index(analyzed_wordform_id);
+      |drop table if exists simple_analyzed_wordforms;
+      |create table simple_analyzed_wordforms
+      |select * from analyzed_wordforms
+      |where not (analyzed_wordform_id in (select analyzed_wordform_id from part_of_group));
+      |alter table simple_analyzed_wordforms add index(analyzed_wordform_id);
+      |alter table simple_analyzed_wordforms add index(lemma_id);
+      |alter table simple_analyzed_wordforms add index(wordform_id);
+      |
+    """.stripMargin
 }
 
 object Hilex {
