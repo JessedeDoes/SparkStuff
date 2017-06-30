45a78da1a3f0585a0dc0b4cee3a9ca5104e19c7a
Jesse de Does <jesse.dedoes@inl.nl>
2017-06-30 12:26:50 +0200

    .

--- src/main/scala/LexiconService.scala
+++ src/main/scala/LexiconService.scala
@@ -2,16 +2,25 @@ import scala.xml._
 
 object LexiconService
 {
+   val serviceURL = "http://sk.taalbanknederlands.inl.nl/LexiconService/lexicon"
+   val database = "lexicon_service_db"
+
    def getWordforms(lemma: String):List[String] =
    {
-     (XML.load("http://sk.taalbanknederlands.inl.nl/LexiconService/lexicon/get_wordforms?database=lexicon_service_db&lemma=" 
+     (XML.load(s"$serviceURL/get_wordforms?database=$database&lemma="
          + lemma.toLowerCase)
      \\ "found_wordforms").toList.map(_.text.toLowerCase).filter(_.matches("^(\\p{L})*$"))
    }
    
    def getLemmata(wordform: String):List[String] =
    {
-     (XML.load("http://sk.taalbanknederlands.inl.nl/LexiconService/lexicon/get_lemmata?database=lexicon_service_db&wordform=" + wordform.toLowerCase)
+     (XML.load(s"$serviceURL/get_lemmata?database=$database&wordform=" + wordform.toLowerCase)
      \\ "found_lemmata").toList.map(_.text.toLowerCase)
+   }
+
+   def main(args: Array[String]) : Unit =
+   {
+     getWordforms("wereld").foreach(println)
+     getLemmata("kip").foreach(println)
    }
 }
