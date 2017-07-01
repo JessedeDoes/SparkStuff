d68a16ad4fa50e3eddd880120a69816e92adda55
jessededoes <dedoes@xs4all.nl>
2017-07-01 15:57:13 +0200

    .

--- src/main/scala/LexiconService.scala
+++ src/main/scala/LexiconService.scala
@@ -1,26 +1,34 @@
+import LexiconService.{database, serviceURL}
+
 import scala.xml._
 
-object LexiconService
+case class LexiconService(serviceURL: String, database: String)
 {
-   val serviceURL = "http://sk.taalbanknederlands.inl.nl/LexiconService/lexicon"
-   val database = "lexicon_service_db"
+  def getWordforms(lemma: String):List[String] =
+  {
+    (XML.load(s"$serviceURL/get_wordforms?database=$database&lemma="
+      + lemma.toLowerCase)
+      \\ "found_wordforms").toList.map(_.text.toLowerCase).filter(_.matches("^(\\p{L})*$"))
+  }
 
-   def getWordforms(lemma: String):List[String] =
-   {
-     (XML.load(s"$serviceURL/get_wordforms?database=$database&lemma="
-         + lemma.toLowerCase)
-     \\ "found_wordforms").toList.map(_.text.toLowerCase).filter(_.matches("^(\\p{L})*$"))
-   }
-   
-   def getLemmata(wordform: String):List[String] =
-   {
-     (XML.load(s"$serviceURL/get_lemmata?database=$database&wordform=" + wordform.toLowerCase)
-     \\ "found_lemmata").toList.map(_.text.toLowerCase)
-   }
+  def getLemmata(wordform: String):List[(String,String)] =
+  {
+    (XML.load(s"$serviceURL/get_lemma?database=$database&wordform=" + wordform.toLowerCase)
+      \\ "found_lemmata").toList.map(e => ((e \ "lemma").text, (e \ "pos").text))
+  }
+}
 
+object defaults
+{
+  val serviceURL = "http://sk.taalbanknederlands.inl.nl/LexiconService/lexicon"
+  val database = "lexicon_service_db"
+}
+
+object LexiconService extends LexiconService(defaults.serviceURL, defaults.database)
+{
    def main(args: Array[String]) : Unit =
    {
      getWordforms("wereld").foreach(println)
-     getLemmata("kip").foreach(println)
+     getLemmata("kipt").foreach(println)
    }
 }
